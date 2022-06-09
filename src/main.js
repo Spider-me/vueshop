510120204317b777a9c22b824ca3e04961b06cda
Spider-Me <983500351@qq.com>
2022-06-09 18:19:58 +0800

    登录完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "./assets/global.css"
+
+import "./assets/fonts/iconfont.css"
+import 'element-ui/lib/theme-chalk/index.css';
+import {
+	Form,
+	FormItem,
+	Input,
+	Button,
+	Message
+} from 'element-ui'
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Button)
+Vue.prototype.$message = Message
+
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios;
+
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+	router,
+	render: h => h(App)
 }).$mount('#app')
