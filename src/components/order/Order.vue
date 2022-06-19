64757d5d91349a32ced0db56c6eeaf450cf295d2
Spider-Me <983500351@qq.com>
2022-06-19 19:19:44 +0800

    订单部分功能完成

---
+++ src/components/order/Order.vue
@@ -0,0 +1,163 @@
+<template>
+	<div>
+		<el-breadcrumb separator="/">
+			<el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+			<el-breadcrumb-item><a href="/">订单管理</a></el-breadcrumb-item>
+			<el-breadcrumb-item>订单列表</el-breadcrumb-item>
+		</el-breadcrumb>
+		<el-card class="box-card">
+			<el-row :gutter="20">
+				<el-col :span="7">
+					<el-input placeholder="请输入内容" class="input-with-select" v-model="queryInfo.query" clearable
+						@clear="getOrderlist">
+						<el-button @click="getOrderlist" slot="append" icon="el-icon-search"></el-button>
+					</el-input>
+				</el-col>
+				<el-col :span="4">
+					<el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
+				</el-col>
+			</el-row>
+			<el-table border stripe :data="ordersList">
+				<el-table-column type="index" label="#">
+				</el-table-column>
+				<el-table-column prop="order_number" label="订单编号">
+				</el-table-column>
+				<el-table-column prop="order_price" label="订单价格">
+				</el-table-column>
+				<el-table-column prop="pay_status" label="是否付款">
+					<template slot-scope="scope">
+						<el-tag type="success" v-if="scope.row.pay_status == '1'">已付款</el-tag>
+						<el-tag type="danger" v-else>未付款</el-tag>
+					</template>
+				</el-table-column>
+				<el-table-column prop="is_send" label="是否发货">
+				</el-table-column>
+				<el-table-column label="下单时间">
+					<template slot-scope="scope">
+						{{scope.row.create_time | dateFormat}}
+					</template>
+				</el-table-column>
+				<el-table-column label="操作">
+					<template slot-scope="scope">
+						<el-tooltip effect="dark" content="修改订单地址" placement="top" :enterable="false">
+							<el-button size="mini" @click="showBox(scope.row)" type="primary" icon="el-icon-edit">
+							</el-button>
+						</el-tooltip>
+						<el-tooltip effect="dark" content="物流进度" placement="top" :enterable="false">
+							<el-button size="mini" @click="showProgressBox" type="success" icon="el-icon-location">
+							</el-button>
+						</el-tooltip>
+					</template>
+				</el-table-column>
+			</el-table>
+			<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+				:current-page="queryInfo.pagenum" :page-sizes="[10, 15, 20, 25]" :page-size="queryInfo.pagesize"
+				layout="total, sizes, prev, pager, next, jumper" :total="total">
+			</el-pagination>
+		</el-card>
+
+		<el-dialog title="修改地址" :visible.sync="addDialogVisible" width="50%" @close="addDialogClosed">
+			<div>
+				<el-form :model="addressForm" :rules="addressFormrules" ref="addressFormref" label-width="100px">
+					<el-form-item label="省市区/县" prop="address1">
+						<el-cascader :options="cityData" v-model="addressForm.address1"></el-cascader>
+					</el-form-item>
+					<el-form-item label="详细地址" prop="address2">
+						<el-input v-model="addressForm.address2"></el-input>
+					</el-form-item>
+				</el-form>
+			</div>
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="addDialogVisible = false">取 消</el-button>
+				<el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+			</span>
+		</el-dialog>
+
+		<el-dialog title="物流进度" :visible.sync="showProgressVisible" width="50%">
+			<div>
+				<el-timeline :reverse="true">
+					<el-timeline-item v-for="(activity, index) in progressInfo" :key="index"
+						:timestamp="activity.ftime">
+						{{activity.context}}
+					</el-timeline-item>
+				</el-timeline>
+			</div>
+		</el-dialog>
+	</div>
+</template>
+
+<script>
+	import kuaidi from './interface/kuaidi.js'
+	import cityData from './citydata.js'
+	export default {
+		data() {
+			return {
+				queryInfo: {
+					query: "",
+					pagenum: 1,
+					pagesize: 10
+				},
+				ordersList: [],
+				total: 0,
+				addDialogVisible: false,
+				addressForm: {
+					address1: [],
+					address2: ""
+				},
+				addressFormrules: {
+					address1: [{
+						required: true,
+						message: "请选择省市区/县",
+						trigger: 'blur'
+					}],
+					address2: [{
+						required: true,
+						message: "请输入详细地址",
+						trigger: 'blur'
+					}]
+				},
+				cityData,
+				showProgressVisible: false,
+				progressInfo: kuaidi
+			}
+		},
+		created() {
+			this.getOrderlist()
+		},
+		methods: {
+			async getOrderlist() {
+				const {
+					data: res
+				} = await this.$http.get('orders', {
+					params: this.queryInfo
+				});
+				if (res.meta.status !== 200) return this.$message.error('获取用户列表失败!')
+				this.ordersList = res.data.goods;
+				this.total = res.data.total;
+			},
+			handleSizeChange(newsize) {
+				this.queryInfo.pagesize = newsize;
+				this.getOrderlist()
+			},
+			handleCurrentChange(newpage) {
+				this.queryInfo.pagenum = newpage;
+				this.getOrderlist()
+			},
+			showBox() {
+				this.addDialogVisible = true;
+			},
+			addDialogClosed() {
+				this.$refs.addressFormref.resetFields()
+			},
+			showProgressBox() {
+				this.showProgressVisible = true;
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.el-cascader {
+		width: 100%;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -43,7 +43,9 @@ import {
 	Step,
 	Checkbox,
 	CheckboxGroup,
-	Upload
+	Upload,
+	Timeline,
+	TimelineItem
 } from 'element-ui'
 Vue.use(Form)
 Vue.use(FormItem)
@@ -80,6 +82,8 @@ Vue.use(Step)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
 
@@ -103,6 +107,21 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 Vue.use(VueQuillEditor)
+
+
+Vue.filter('dateFormat', function(originVal) {
+	const dt = new Date(originVal)
+
+	const y = dt.getFullYear()
+	const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+	const d = (dt.getDate() + '').padStart(2, '0')
+
+	const hh = (dt.getHours() + '').padStart(2, '0')
+	const mm = (dt.getMinutes() + '').padStart(2, '0')
+	const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+	return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 Vue.config.productionTip = false
 
